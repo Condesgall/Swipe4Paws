6a6ff1e8cbfd2ff7249245710b35edf999df6106
Condegall <sandrag@outlook.dk>
2024-11-13 16:15:43 +0100

    shelter correct

--- frontend/src/app/shelters/shelters-view/shelters.component.ts
+++ frontend/src/app/shelters/shelters-view/shelters.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { PetDataService } from '../../pet-data/pet-data.service';
-import { PetsListing } from '../../models/pets-listing';
 
 @Component({
   selector: 'app-shelters',
@@ -9,24 +7,4 @@ import { PetsListing } from '../../models/pets-listing';
   templateUrl: './shelters.component.html',
   styleUrl: './shelters.component.css'
 })
-export class SheltersComponent {
-  pet: PetsListing = {
-    id: 0,
-    name: '',
-    age: 0,
-    type: '',
-    gender: '',
-    behavior: [],
-    city: '',
-    postDate: new Date(),
-    photo: ''
-  };
-
-  constructor(private petDataService: PetDataService) {}
-
-  onSubmit(): void {
-    this.pet.id = Date.now();
-    this.petDataService.addPet(this.pet);
-    this.pet = { ...this.pet, id: 0, name: '', age: 0, type: '', gender: '', behavior: [], city: '', photo: ''};
-  }
-}
+export class SheltersComponent {};
